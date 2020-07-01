b4d13fd624d97188f65a3b12652b68485e6083b7
John Manouse <johnmanouse@Macintosh-2.local>
2020-07-01 12:21:08 -0400

    link api's

--- script.js
+++ script.js
@@ -1,57 +1,77 @@
 //Run page when document is loaded
-$(document).ready(function() {
+$(document).ready(function () {
 
-    //API Key
-    const appID = "de153e330cd6475e4d72b5a0d24f06ce";
+  //API Key
+  const appID = "de153e330cd6475e4d72b5a0d24f06ce";
 
-    //Search Button function for City Search
-    $(".btn btn-primary").click(function(){
-        let query_param = $(this).prev().val();
+  //Search Button function for City Search
+  $("#cityInputButton").click(function (e) {
+    // Prevents page from refreshing when submit buttons are clicked
+    e.preventDefault();
 
-        //Input Placeholder including previous city search
-        ($(this).prev().attr("placeholder") == "City") {
-          let weather = "api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid=" + query_param + "&APPID=" + appID;
-      }
-        //Load json data from api
-        $.getJSON(weather,function(json){
-            $("#city").html(json.name);
-            $("#temperature").html(json.main.temp);
-            $("#humidity").html(json.main.humidity);
-            $("#wind speed").html(json.main.windSpeed);
-            $("#uvIndex").html(json.uvIndex);
+    let query_param = $(this).prev().val();
 
-            $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
-            
-          });
-        })
-             // Code for temperature conversion
-    let fahrenheit = true;
+    if (query_param.length === 0) {
+      console.log("Please enter a city")
+      return
+    };
 
-    $("#convertToCelsius").click(function() {
-        if (fahrenheit) {
-            $("#temperature").text(((($("#temperature").text() - 32) * 5) / 9));
-        }
-        fahrenheit = false;
-    });
+    console.log(`You are searching for the weather of city ${query_param}`)
+    $.get(`https://api.openweathermap.org/data/2.5/weather?q=${query_param}&appid=${appID}`)
+      .then((response) => response)
+      .then((weatherData) => {
 
-    $("#convertToFahrenheit").click(function() {
-        if (fahrenheit == false) {
-            $("#temperature").text((($("#temperature").text() * (9/5)) + 32));
-        }
-        fahrenheit = true;
-    })
-});
+        const {
+          name,
+          main,
+          weather,
+          wind,
+          coord
+        } = weatherData;
 
-$.get("api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid=" + query_param + "&APPID=" + appID)
-  .then((res) => {
-    console.log("First callback");
+        const {
+          temp,
+          humidity
+        } = main;
+
+        $("#city").html(name);
+        $("#temperature").html(temp);
+        $("#humidity").html(humidity);
+        $("#wind-speed").html(wind.speed);
+
+        $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + weather[0].icon + ".png");
+
+        return coord;
+      })
+      .then(({
+        lat,
+        lon
+      }) => {
+        // #uvIndex
+        // Retrieve UV index
+        $.get(`https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${appID}`)
+          .then(({
+            value
+          }) => $("#uvIndex").html(value))
+
+      })
+
   })
-  .then((res) => {
-    console.log("Second callback");
-  })
-  .then((res) => {
-    console.log("Third callback");
+
+  // Code for temperature conversion
+  let fahrenheit = true;
+
+  $("#convertToCelsius").click(function () {
+    if (fahrenheit) {
+      $("#temperature").text(((($("#temperature").text() - 32) * 5) / 9));
+    }
+    fahrenheit = false;
   });
 
-    console.log("Test");
-
+  $("#convertToFahrenheit").click(function () {
+    if (fahrenheit == false) {
+      $("#temperature").text((($("#temperature").text() * (9 / 5)) + 32));
+    }
+    fahrenheit = true;
+  })
+});
