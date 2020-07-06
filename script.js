2e5e4c3d0fe7ec54b554450c3accd54ef7600904
John Manouse <johnmanouse@Macintosh-2.local>
2020-07-06 11:37:12 -0400

    update script error

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //Run page when document is loaded
 $(document).ready(function () {
-
   //API Key
   const appID = "de153e330cd6475e4d72b5a0d24f06ce";
 
@@ -12,110 +11,68 @@ $(document).ready(function () {
     let query_param = $(this).prev().val();
 
     if (query_param.length === 0) {
-      console.log("Please enter a city")
-      return
-    };
+      console.log("Please enter a city");
+      return;
+    }
 
-    console.log(`You are searching for the weather of city ${query_param}`)
-    $.get(`https://api.openweathermap.org/data/2.5/weather?q=${query_param}&appid=${appID}`)
+    console.log(`You are searching for the weather of city ${query_param}`);
+    $.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${query_param}&appid=${appID}`
+    )
       .then((response) => response)
       .then((weatherData) => {
+        const { name, main, weather, wind, coord } = weatherData;
 
-        const {
-          name,
-          main,
-          weather,
-          wind,
-          coord
-        } = weatherData;
-
-        const {
-          temp,
-          humidity
-        } = main;
+        const { temp, humidity } = main;
 
         $("#city").html(name);
-        $("#temperature").html(temp);
+        const convertedTemp = (parseFloat(temp) - 273.15) * (9 / 5) + 32;
+        $("#temperature").html(`${convertedTemp.toFixed(1)} °F`);
         $("#humidity").html(humidity);
         $("#wind-speed").html(wind.speed);
 
-        $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + weather[0].icon + ".png");
+        $("#weather_image").attr(
+          "src",
+          "http://openweathermap.org/img/w/" + weather[0].icon + ".png"
+        );
 
         return coord;
       })
-      .then(({
-        lat,
-        lon
-      }) => {
-        
+      .then(({ lat, lon }) => {
         // #uvIndex
         // Retrieve UV index
-        $.get(`https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${appID}`)
-          .then(({
-            value
-          }) => $("#uvIndex").html(value))
-          function styleUvIndex(uv){
-            var colors;
-        
-            if (uv < 3) {
-              colors = '#458b00';
-            }
-            else if (uv < 6) {
-                colors = '#ffd700';
-            }
-            else if (uv < 8) {
-                colors = '#ee7600'
-            }
-            else if (uv < 11) {
-                colors = '#cd2626'
-            }
-            else {
-                colors = '#9400d3'
-            }
-        
-            $("#uvIndex").css("background-color", colors);
-        
-        }
-      })
-      // 5 Day Forecast
-        $.get(`https://api.openweathermap.org/data/2.5/forecast?q=${lat}&lon=${lon}&appid=${appID}`)
-        .then((response) => response)
-      .then((fiveDay) => {
+        $.get(
+          "https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${appID}")
+        .then(({ value }) => $("#uvIndex").html(value));
+        function styleUvIndex(uv) {
+          var colors;
 
-        const {
-          name,
-          date5,
-          img5,
-          coord
+          if (uv < 3) {
+            colors = "#458b00";
+          } else if (uv < 6) {
+            colors = "#ffd700";
+          } else if (uv < 8) {
+            colors = "#ee7600";
+          } else if (uv < 11) {
+            colors = "#cd2626";
+          } else {
+            colors = "#9400d3";
+          }
+
+          $("#uvIndex").css("background-color", colors);
+        }
+      });
+      
+    // 5 Day Forecast
+    $.get(
+      "https://api.openweathermap.org/data/2.5/forecast?q=${lat}&lon=${lon}&appid=${appID}"
+    )
+      .then((response) => response)
+      .then((fiveDay) => {
+        const {list
         } = fiveDay;
 
-        const {
-          temp5,
-          humidity5
-        } = forecast;
-
-        $("#date5").html(date5);
-        $("#img5").html(img5);
-        $("#temp5").html(temp5);
-        $("#humidity5").html(humidity5);
-        
-        return coord;
+        const sliceArray = list.slice(0,5);
       })
-      .then(({
-        lat,
-        lon
-      }) => {
-        
-      })
-  })
-
-  // Code for temperature conversion
-  let fahrenheit = true;
-
-  $("#convertToFahrenheit").click(function () {
-    if (fahrenheit == false) {
-      $("#temperature").text((($("#temperature").text() * (9 / 5)) + 32));
-    }
-    fahrenheit = true;
-  })
-});
+    })
+  });
